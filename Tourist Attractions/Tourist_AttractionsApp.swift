6e9f6fd6f4b59c8dd2151d7ea79a22002946aa30
Mostafa Ashraf <mostafa.ashraf10497@gmail.com>
2024-01-07 16:42:41 +0200

    Initial Commit

---
+++ Tourist Attractions/Tourist_AttractionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tourist_AttractionsApp.swift
+//  Tourist Attractions
+//
+//  Created by A on 07/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct Tourist_AttractionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
